119f8e645cbc6d0979940c89861abfdfc13b70b2
Bruno Staine <brunostaine@hotmail.com>
2023-10-13 17:46:55 -0300

    Sum two numbers

---
+++ challenger_03.js
@@ -0,0 +1,6 @@
+const sumTwoNumbers = (firstNumber, lastNumber) => {
+  const sum = firstNumber + lastNumber;
+  console.log(`A soma de ${firstNumber} + ${lastNumber} da ${sum}.`);
+};
+
+sumTwoNumbers(326, 2191);
